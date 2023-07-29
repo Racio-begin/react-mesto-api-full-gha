dfecc7795a8c920cf21581818720e9eefc513fce
Nikita Averyanov <rac1onal@yandex.ru>
2023-07-29 17:08:27 +0300

    testinit: просто зафиксил

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -7,7 +7,7 @@
 // export const baseCohortUrl = 'https://mesto.nomoreparties.co/v1/cohort-64';
 
 // API-адрес для получения данных пользователя с моего сервера (локальная машина)
-// export const BASE_URL = "http://localhost:3000"
+export const BASE_URL = "http://localhost:3000"
 
 // API-адрес для получения данных пользователя с моего сервера
-export const BASE_URL = "http://api.giga-mesto.nomoredomains.xyz"
+// export const BASE_URL = "http://api.giga-mesto.nomoredomains.xyz"
