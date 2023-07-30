f045624589c9f4e8db1d9ecc28293a6af29db9d4
Nikita Averyanov <rac1onal@yandex.ru>
2023-07-30 22:46:13 +0300

    sryle: навести красоту

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,12 +1,5 @@
 //*	API-константы	*//
 
-// API-адрес для авторизации и регистрации пользователя
-// export const BASE_URL = 'https://auth.nomoreparties.co/';
-
-// API-адрес и личный токен для получения данных пользователя с сервера Яндекса
-// export const baseCohortUrl = 'https://mesto.nomoreparties.co/v1/cohort-64';
-// export const myToken = 'c467341b-672a-48a9-be5b-ea4a89cc731a';
-
 // API-адрес для получения данных пользователя с моего сервера (локальная машина)
 // export const BASE_URL = "http://localhost:3000"
 
