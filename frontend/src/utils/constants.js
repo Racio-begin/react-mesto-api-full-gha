676bd812bfe4d4703e48dfd3fd37d239318416e9
Nikita Averyanov <rac1onal@yandex.ru>
2023-08-02 19:30:45 +0300

    init: проверить работоспособность сервера

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,7 +1,7 @@
 //*	API-константы	*//
 
 // API-адрес для получения данных пользователя с моего сервера (локальная машина)
-export const BASE_URL = "http://localhost:3000"
+// export const BASE_URL = "http://localhost:3000"
 
 // API-адрес для получения данных пользователя с моего сервера
-// export const BASE_URL = "https://api.giga-mesto.nomoredomains.xyz"
+export const BASE_URL = "https://api.giga-mesto.nomoredomains.xyz"
