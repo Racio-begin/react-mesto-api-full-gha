956d2f159dc7908095a46a9475fa43df711d5d3d
Nikita Averyanov <rac1onal@yandex.ru>
2023-08-02 20:39:47 +0300

    hotfix: убрать лишний код из app.js

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,17 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
 
 const cors = require('cors');
-const NotFoundError = require('./errors/NotFoundError');
-// eslint-disable-next-line import/no-unresolved
+
+const routes = require('./routes/index');
+
 const errorHandler = require('./errors/ErrorHandler');
 
-const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const { createUserJoiValidation, loginJoiValidation } = require('./middlewares/JoiValidator');
-
-const usersRouter = require('./routes/users');
-const cardsRouter = require('./routes/cards');
-
-const { createUser, login, logout } = require('./controllers/users');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
@@ -45,24 +39,8 @@ app.use(express.json());
 // подключаем логгер запросов
 app.use(requestLogger);
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
-app.post('/signin', loginJoiValidation, login);
-app.post('/signup', createUserJoiValidation, createUser);
-app.post('/signout', logout);
-
-app.use(auth);
-
-app.use('/users', usersRouter);
-app.use('/cards', cardsRouter);
-
-app.use('*', (req, res, next) => {
-  next(new NotFoundError('Ресурс не найден. Проверьте правильность введенного URL.'));
-});
+// подключаем роуты
+app.use(routes);
 
 // подключаем логгер ошибок
 app.use(errorLogger);
